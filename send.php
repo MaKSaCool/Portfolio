bfcea969dd18106202731fde11d071779bf28279
Maksym <53348785+MaKSaCool@users.noreply.github.com>
2021-11-17 10:44:27 +0200

    Update send.php

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
     $name = $_POST['name'];
     $text = $_POST['text'];
     $email = $_POST['email'];
-    $token = "1427753648:AAGdxmH1_OYllLU8ZHGIQGcWZ99RDVeBub8";
+    $token = "";
     $chat_id = "-1001370030402";
     $arr = array(
         'Имя: ' => $name,
